927a4d6c17de49fa7bbd95f778d60d71c678b5f5
Ines Louro <inespazlouro@gmail.com>
2021-09-06 16:13:30 +0100

    fixed some errors in CRUD pets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_144605) do
+ActiveRecord::Schema.define(version: 2021_09_06_150736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_144605) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "pet_type"
     t.boolean "on_heat"
+    t.string "breed"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
