cff12d79ee85ef5223a85bcf45ef7012f7696c12
catia falagueira <catia.falagueira@gmail.com>
2021-09-06 15:19:46 +0100

    [Add] seeds

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,3 @@
 class PetsController < ApplicationController
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts '*****Creating 30 pets*****'
+
+30.times do
+pet = Pet.new(
+  pet_type: ["cat", "dog"].sample,
+  name: ["Mel", "Gucci", "Sebastião", "Pucci", "Fendi", "Celine", "Dior", "Pearl", "Gina", "Lua", "Batman", "Wintour",
+         "Marc", "Kim", "Nívea", "Xena", "Kiki", "Eva", "Agnes", "Hera", "Patty", "Penny", "Bonnie", "Alice", "Dalila",
+         "Jean-Paul", "Westwood", "Pucci", "Wang", "Ballmer"].sample,
+  sex: ["male", "female"].sample,
+  pedigree_number: Faker::IDNumber.valid,
+  description: " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+)
+pet.save!
+end
+puts '******Finished!'
