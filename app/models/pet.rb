51dee4928f5c2c8dec6e7025277070bf552bf14c
Ines Louro <inespazlouro@gmail.com>
2021-09-07 12:22:28 +0100

    added model validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,17 @@
 class Pet < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+
+  validates :name, :sex, :photo, :breed, :pet_type, presence: true
+  validates :pedigree_number, uniqueness: true
+
+  after_initialize :set_default
+
+  def set_default
+    if self.sex == "male"
+      self.on_heat = true
+    else
+      self.on_heat = false
+    end
+  end
 end
