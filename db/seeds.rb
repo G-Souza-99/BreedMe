0d1edfc333790d2f2d0714a46fba40279d7fa2d6
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-08 17:14:42 +0100

    removed require bye bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-require 'byebug'
 
 puts '***** Destroying Seeds *****'
 User.destroy_all
