23b694819240a12ded583ec1ab80d3559472b684
Ines Louro <inespazlouro@gmail.com>
2021-09-08 12:11:49 +0100

    user can upload profile pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :photo
   has_many :pets, dependent: :destroy
   has_many :messages
   has_many :chatrooms
