df603bad09c151cd40f865843661253806eff19c
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-13 17:22:22 +0100

    switch working with thingy

--- app/javascript/components/initSelect2.js
+++ app/javascript/components/initSelect2.js
@@ -1,21 +1,6 @@
 import $ from "jquery";
 import "select2";
 
-//    console.log("dog");
-//   $(".dog").select2({
-//     placeholder: "Search for breeds",
-//     minimumResultsForSearch: 1,
-//   });
-// };
-// const initSelect3 = () => {
-//   console.log("cat");
-//   $(".cat").select2({
-//     placeholder: "Search for breeds",
-//     minimumResultsForSearch: 1,
-//   });
-// };
-
-
 const initSelect2 = () => {
 
 if ($('.dog').length > 0) {
