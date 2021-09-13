727f8c256e5272c70bd7256636c2c4b895c0782f
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-13 18:35:57 +0100

    all front end working, @louro-ines fix the rest pls👼😇😇

--- app/javascript/components/initSelect2.js
+++ app/javascript/components/initSelect2.js
@@ -4,11 +4,15 @@ import "select2";
 const initSelect2 = () => {
 
 if ($('.dog').length > 0) {
-  $('.dog').select2();
+  $('.dog').select2({
+    placeholder: "search for a dog breed"
+  });
 };
 
 if ($('.cat').length > 0) {
-  $('.cat').select2();
+  $('.cat').select2({
+    placeholder: "search for a cat breed"
+  });
 }; }
 
 export { initSelect2 };
