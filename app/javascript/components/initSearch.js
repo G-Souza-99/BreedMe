e1d1d6642a5a25042fab1a6a3b1cbbf967bcc870
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-15 15:38:44 +0100

    minimal styling

--- app/javascript/components/initSearch.js
+++ app/javascript/components/initSearch.js
@@ -1,19 +1,13 @@
 const initSearch = () => {
   const searchIcon = document.querySelector('.fa-search');
   let searchArray = []; // Array that will be pushing the info to be searched
-
-
   let count = 0;
   let href =  "?";
-
   const btnToHide = document.getElementById("btn-to-hide");
-  console.log(btnToHide);
-
   if (searchIcon) {
     searchIcon.addEventListener('click', () => {
       console.log("You Clicked on the button");
       let searchTexts = document.querySelectorAll(".select2-selection__choice"); // Value that is searched
-
       searchTexts.forEach((searchText) => {
         count += 1;
         searchArray.push(searchText.title);
